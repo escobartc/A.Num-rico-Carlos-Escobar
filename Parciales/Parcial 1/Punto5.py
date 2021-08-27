30eb860b2ac4f300590b3bc53ca9eca8d40d4567
escobartc <carlosescobartriana@gmail.com>
2021-08-27 09:04:08 -0500

    Solucion parcial
    Se me olvido comentar el punto 5 jaja

--- Parciales/Parcial 1/Punto5.py
+++ Parciales/Parcial 1/Punto5.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 # Método de Newton-Raphson
 
-# INGRESO
+# INGRESO se definen la función y su derivada
 fx = lambda x: np.longdouble(np.exp(x) - x - 1)
 dfx = lambda x: np.longdouble(np.exp(x) - 1)
 
@@ -13,22 +13,24 @@ x0 = 1
 tolera = 1e-8
 
 # PROCEDIMIENTO
+# se crea una tabla para meter los valores que imprimiremos mas adelante, xi, xnuevo y su error
 tabla = []
 error = abs(2*tolera)
 xi = x0
 it = 0
+#Se realiza el ciclo del metodo de newton
 while (error>=tolera):
     xnuevo = xi - fx(xi)/dfx(xi)
     error = abs(xnuevo-xi)
     it = it + 1
-    tabla.append([xi,xnuevo,error])
+    tabla.append([xi,xnuevo,error]) #Se agregan a la tabla los valores
     xi = xnuevo
 
 # convierte la lista a un arreglo.
 tabla = np.array(tabla)
 n=len(tabla)
 
-
+#Se hace el format de como queremos que se impriman las raíces usando la tolerancia
 formato = "{:."+str(tolera).split('-')[1]+"f}"
 # SALIDA
 print(['xi', 'xnuevo', 'error'])
@@ -36,6 +38,7 @@ np.set_printoptions(precision = 4)
 print(tabla)
 print('raiz en: ', formato.format(xi))
 print('con error de: ',error)
+
 
 x = []
 y = []
@@ -48,7 +51,7 @@ while m < n:
     #print(x[m],"  ",y[m])
     m = m + 1
     
-    
+#Se procede a imprimir la gráfica
 plt.plot(x, z, label = "xi")
 plt.plot(y, z, label = "xnuevo")
 plt.legend()  
